f0d98c48ca57931d1ac3c3f9910803dc5a151dd9
Gogon <sh.setyana@gmail.com>
2018-07-02 15:20:11 +0700

    Update config.sh and module.prop

--- config.sh
+++ config.sh
@@ -41,7 +41,7 @@ LATESTARTSERVICE=false
 
 print_modname() {
   ui_print "*******************************"
-  ui_print "     Camera2 API enabler       "
+  ui_print "   Camera2 API enabler Fix     "
   ui_print "*******************************"
 }
 
